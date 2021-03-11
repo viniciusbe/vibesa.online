fde57aa9f748d6ccf501ed25707117ff9df94f73
viniciusbe <vinicius.bernardesds@gmail.com>
2021-03-11 03:21:19 -0300

    Initial commit

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,41 +1,50 @@
-import React, { ReactNode } from 'react'
-import Link from 'next/link'
-import Head from 'next/head'
+import React, { ReactNode } from 'react';
+import Link from 'next/link';
+import Head from 'next/head';
+import Image from 'next/image';
 
 type Props = {
-  children?: ReactNode
-  title?: string
-}
+  children?: ReactNode;
+  title?: string;
+};
 
-const Layout = ({ children, title = 'This is the default title' }: Props) => (
-  <div>
+const Layout = ({
+  children,
+  title = 'This is the default title',
+}: Props): JSX.Element => (
+  <div className="w-screen h-screen grid grid-cols-2 bg-gray-700 text-white">
     <Head>
       <title>{title}</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
-    <header>
-      <nav>
+    <aside
+      className="w-32 h-screen flex flex-col justify-between align-center
+      p-4 border-r-2 border-red-800 bg-gray-800"
+    >
+      <Image src="/logo.svg" alt="Logo" width={135} height={50} />
+      <nav className="flex flex-col flex-grow justify-center text-center">
         <Link href="/">
-          <a>Home</a>
-        </Link>{' '}
-        |{' '}
+          <a className="py-4 border-b border-red-800">about</a>
+        </Link>
         <Link href="/about">
-          <a>About</a>
-        </Link>{' '}
-        |{' '}
+          <a className="py-4 border-b border-red-800">portfolio</a>
+        </Link>
+        <Link href="/about">
+          <a className="py-4 border-b border-red-800">skills</a>
+        </Link>
         <Link href="/users">
-          <a>Users List</a>
-        </Link>{' '}
-        | <a href="/api/users">Users API</a>
+          <a className="py-4 ">contact</a>
+        </Link>
       </nav>
-    </header>
-    {children}
-    <footer>
-      <hr />
-      <span>I'm here to stay (Footer)</span>
-    </footer>
+      <div>
+        <Image src="/in.svg" alt="Logo" width={30} height={30} />
+        <Image src="/github-icon.svg" alt="Logo" width={30} height={30} />
+        <Image src="/twitter.svg" alt="Logo" width={30} height={30} />
+      </div>
+    </aside>
+    <main>{children}</main>
   </div>
-)
+);
 
-export default Layout
+export default Layout;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,12 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import Link from 'next/link';
+import React from 'react';
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
-
-export default IndexPage
+const IndexPage = (): JSX.Element => {
+  return (
+    <div>
+      <h1>Conteúdo</h1>
+      <h2>sadsa</h2>
+    </div>
+  );
+};
+export default IndexPage;
