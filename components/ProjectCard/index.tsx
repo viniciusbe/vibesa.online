c21c15b98647207a39afe2de940c68a3bc41395b
viniciusbe <vinicius.bernardesds@gmail.com>
2021-03-17 23:41:05 -0300

    refactor : style ProjectCard

--- components/ProjectCard/index.tsx
+++ components/ProjectCard/index.tsx
@@ -2,10 +2,12 @@ import Image from 'next/image';
 
 const ProjectCard = (): JSX.Element => {
   return (
-    <div>
+    <div className="border border-gray-lighter rounded px-8 py-4">
       <h1>vibesa.online</h1>
       <p>Esse portfolio</p>
-      <Image src="/vibesa.png" width={300} height={300} />
+      <figure className="pt-2">
+        <Image src="/vibesa.png" width={300} height={300} />
+      </figure>
     </div>
   );
 };
